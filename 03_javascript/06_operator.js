bedd652595c8a9b313b2d3bc98562c414f029bec
Ping Zhang <pingzhang9595@gmail.com>
2024-06-11 00:21:53 -0700

    operators

---
+++ 03_javascript/06_operator.js
@@ -0,0 +1,33 @@
+// arithmetic operator
+// + - * / % 
+// ++ --
+// relational operator
+// > < >= <= 
+// != == 
+// !== ===
+// logical operator
+// && || !
+// conditional operator
+// ? :
+// bitwise operator
+// & | ^ ~ << >> >>>
+// assignment operator
+// = 
+
+
+console.log("arithmetic operator");
+console.log("10/2 = " + 10/2);  // 5
+console.log("10/4 = " + 10/4);  // 2.5
+console.log("10/0 = " + 10/0);  // Infinity
+console.log("10%0 = " + 10%0);  // NaN = not a number
+
+console.log("\nrelational operator");
+console.log("1==1 is " + (1==1));  // true
+console.log("1===1 is " + (1===1));  // true
+console.log("1=='1' is " + (1=='1'));  // true
+console.log("1==='1' is " + (1==='1'));  // false
+console.log("0==false is " + (0==false));  // true
+console.log("1==true is " + (1==true));  // true
+console.log("1===true is " + (1===true));  // false
+
+
